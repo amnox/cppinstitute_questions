1b5ea81dd668bd2f978149950b2835687fdc1025
Aman Khalid <mailamankhalid@gmail.com>
2019-06-04 21:03:07 +0530

    Shorthands

---
+++ post_pre.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+       int counter = 5;
+
+       while(counter) {
+              counter--;
+                cout << "I am an awesome program" << counter << endl;
+                
+       }
+       return 0;
+
+}
