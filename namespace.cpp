483e0493e9cb983047d2f1e1d5c4c85c2a3c880e
Aman <mailamankhalid@gmail.com>
2019-06-14 17:32:44 +0530

    Strings

---
+++ namespace.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std;
+
+namespace Hogwarts {
+	int Troll = 1;
+}
+
+namespace Mordor {
+	int Troll = 2 ;
+}
+
+int main(void) {
+	cout << Hogwarts::Troll << " " << Mordor::Troll << endl;
+	return 0;
+}
