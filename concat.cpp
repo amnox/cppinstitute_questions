cda3e35a58cd903c0681529d75741eac18f4eb42
Aman <mailamankhalid@gmail.com>
2019-06-14 12:39:51 +0530

    Strings

---
+++ concat.cpp
@@ -0,0 +1,11 @@
+#include  <iostream>
+#include  <string> 
+
+using namespace std; 
+
+int main(void) { 
+        string TheGood = "Jekyll", TheBad = "Hyde"; 
+        cout << TheGood + " & " + TheBad << endl; 
+        cout << TheBad + " & " + TheGood << endl; 
+        return 0; 
+} 
