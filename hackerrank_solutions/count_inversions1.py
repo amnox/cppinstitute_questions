f2e4bb4003b8ce75dc2d5c873018a6b031a60fdf
Aman Khalid <aman.khalid@slu.edu>
2020-12-19 22:11:25 -0600

    Count Inversions: n^2

---
+++ hackerrank_solutions/count_inversions1.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countInversions function below.
+def countInversions(arr):
+    inv_count = 0
+    n = len(arr)
+    for i in range(n): 
+        for j in range(i + 1, n): 
+            if (arr[i] > arr[j]): 
+                inv_count += 1
+  
+    return inv_count 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = countInversions(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
