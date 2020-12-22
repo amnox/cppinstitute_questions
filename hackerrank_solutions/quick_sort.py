81687f3477239557d8036b32855bebaaef7abe4a
Aman Khalid <aman.khalid@slu.edu>
2020-12-21 21:38:38 -0600

    Quick Sort

--- hackerrank_solutions/quick_sort.py
+++ hackerrank_solutions/quick_sort.py
@@ -9,12 +9,16 @@ def partition(arr, low, high):
     arr[i+1],arr[high] = arr[high] ,arr[i+1]
     return (i+1)
 
-def quickSort(arr, low, high):
+def quickSort(arr, low, high,level):
+    
     if len(arr) == 1:
         return arr
     if low < high:
         pi = partition(arr, low, high)
-
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
-    
+        print(pi,arr, low, high,level)
+        quickSort(arr, low, pi-1,"first")
+        quickSort(arr, pi+1, high, "second")
+arr = [2,3,5,1,7,6]
+n = len(arr)
+quickSort(arr, 0, n-1,"root")
+print(arr)
