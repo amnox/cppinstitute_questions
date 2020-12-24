910e5cf55710820130edd5336411009b24814aa6
Aman Khalid <aman.khalid@slu.edu>
2020-12-23 20:19:51 -0600

    Alt

---
+++ hackerrank_solutions/max_sum_alternate.py
@@ -0,0 +1,16 @@
+def find_max_sum(arr): 
+	incl = 0
+	excl = 0
+	
+	for i in arr: 
+		
+		# Current max excluding i (No ternary in 
+		# Python) 
+		new_excl = max(excl,incl)
+		
+		# Current max including i 
+		incl = excl + i 
+		excl = new_excl 
+	
+	# return max of incl and excl 
+	return max(excl,incl)
