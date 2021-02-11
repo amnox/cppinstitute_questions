891b5463dcda746e89a80602dd31facc311ac18e
Aman Khalid <aman.khalid@slu.edu>
2021-02-11 17:33:45 -0600

    Practice

---
+++ hackerrank_solutions/a/practice.py
@@ -0,0 +1,14 @@
+import bisect
+
+def pick_box(arr, item):
+    ind = bisect.bisect(arr, item)
+    print(ind,arr)
+    if(ind==len(arr)):
+        return -1
+    else:
+        if(arr[ind-1]==item):
+            return arr[ind-1]
+        else:
+            return arr[ind]
+
+print(pick_box([2, 7, 9, 12,12,13],12))
